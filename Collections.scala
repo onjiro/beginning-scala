c4d772cf4cc74e3303708fdf561c0bb422055f02
onjiro_mohyahya <onjiro.mohyahya@gmail.com>
2011-09-10 22:27:58 +0900

    p.58 Scala の List, Tuple, Option, Map クラス

---
+++ Collections.scala
@@ -0,0 +1,12 @@
+val x = List(1, 2, 3, 4)
+x.filter(a => a % 2 == 0)
+x
+
+val a = Array(1, 2, 3)
+a(1)
+val m = Map("one" -> 1, "two" -> 2, "three" -> 3)
+m("two")
+
+0 to 10
+
+(1 to Integer.MAX_VALUE - 1).take(5)
